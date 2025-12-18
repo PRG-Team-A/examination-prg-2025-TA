44e8775f05d0d658c61507967a876851063daf66
theWasweisich <86789640+theWasweisich@users.noreply.github.com>
2025-12-18 19:10:03 +0100

    initial commit

---
+++ src/main/java/com/prg2025ta/project/examinationpgr2025ta/HelloController.java
@@ -0,0 +1,14 @@
+package com.prg2025ta.project.examinationpgr2025ta;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
