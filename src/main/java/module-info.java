3a0ecc87145a8150973d78312a07b1bc6bd9ef43
Benjamin Lang <benjamin.lang@stud.srh-university.de>
2025-12-18 19:10:03 +0100

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.prg2025ta.project.examinationpgr2025ta {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.prg2025ta.project.examinationpgr2025ta to javafx.fxml;
+    exports com.prg2025ta.project.examinationpgr2025ta;
+}
