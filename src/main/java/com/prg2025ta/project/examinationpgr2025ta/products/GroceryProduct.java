42618b22a95d71447253210e8101459657a0c380
Benjamin Lang <benjamin.lang@stud.srh-university.de>
2026-01-08 10:52:41 +0100

    a basic groceries

---
+++ src/main/java/com/prg2025ta/project/examinationpgr2025ta/products/GroceryProduct.java
@@ -0,0 +1,36 @@
+package com.prg2025ta.project.examinationpgr2025ta.products;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public class GroceryProduct implements Product {
+    private UUID uuid;
+    private double price;
+    private String displayName;
+    private LocalDate dateOfExpiry;
+    private boolean needsCooling;
+
+    @Override public UUID getUuid() { return uuid; }
+    @Override public double getPrice() { return price; }
+    @Override public String getDisplayName() { return displayName; }
+
+    public LocalDate getDateOfExpiry() { return dateOfExpiry; }
+    public boolean needsToBeCooled() { return needsCooling; }
+
+    public void setPrice(double newPrice) { this.price = newPrice; }
+    public void setDisplayName(String newDisplayName) { this.displayName = newDisplayName; }
+
+    public GroceryProduct(String displayName, double price) {
+        this(displayName, price, null);
+    }
+    public GroceryProduct(String displayName, double price, LocalDate dateOfExpiry) {
+        this(displayName, price, dateOfExpiry, false);
+    }
+    public GroceryProduct(String displayName, double price, LocalDate dateOfExpiry, boolean needsCooling) {
+        this.uuid = UUID.randomUUID();
+        this.price = price;
+        this.displayName = displayName;
+        this.dateOfExpiry = dateOfExpiry;
+        this.needsCooling = needsCooling;
+    }
+}
---
+++ src/main/java/com/prg2025ta/project/examinationpgr2025ta/products/Product.java
@@ -0,0 +1,9 @@
+package com.prg2025ta.project.examinationpgr2025ta.products;
+
+import java.util.UUID;
+
+public interface Product {
+    UUID getUuid();
+    double getPrice();
+    String getDisplayName();
+}
