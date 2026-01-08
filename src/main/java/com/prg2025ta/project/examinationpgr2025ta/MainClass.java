678b56b210302903fcd956f8d584b15ce20281eb
Benjamin Lang <benjamin.lang@stud.srh-university.de>
2026-01-08 13:12:43 +0100

    updated pom.xml

---
+++ src/main/java/com/prg2025ta/project/examinationpgr2025ta/MainClass.java
@@ -0,0 +1,7 @@
+package com.prg2025ta.project.examinationpgr2025ta;
+
+public class MainClass {
+    static void main() {
+        throw new UnsupportedOperationException("Main method not yet written");
+    }
+}
