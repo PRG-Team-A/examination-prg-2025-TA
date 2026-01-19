d967a662c28b88f7aba01b3d37be6e356e3b6237
Benjamin Lang <benjamin.lang@srh.university.de>
2026-01-19 11:17:48 +0100

    fixed (?) thread-safety issue

--- src/main/java/com/prg2025ta/project/examinationpgr2025ta/database/SQLiteConnection.java
+++ src/main/java/com/prg2025ta/project/examinationpgr2025ta/database/SQLiteConnection.java
@@ -1,19 +1,21 @@
 package com.prg2025ta.project.examinationpgr2025ta.database;
 
+import org.jetbrains.annotations.Nullable;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class SQLiteConnection implements AutoCloseable {
     public static String dbURL = "jdbc:sqlite:database.db";
-    private static SQLiteConnection INSTANCE = null;
+    private static volatile SQLiteConnection INSTANCE = null;
     private Connection connection = null;
 
     private SQLiteConnection() {}
 
     public Connection getConnection() { return this.connection; }
 
-    public static Connection getInstance() throws SQLException {
+    public synchronized static Connection getInstance() throws SQLException {
         if (INSTANCE == null || INSTANCE.getConnection().isClosed()) {
             INSTANCE = new SQLiteConnection();
             INSTANCE.init();
@@ -29,12 +31,9 @@ public class SQLiteConnection implements AutoCloseable {
         connection = DriverManager.getConnection(SQLiteConnection.dbURL);
     }
 
-    static void main() throws SQLException {
-        DatabaseSetup.setup();
-    }
-
     @Override
     public void close() throws Exception {
         this.connection.close();
+        INSTANCE = null;
     }
 }
