de52a0be5f391b0fc59e379424527ed5edfcc539
Akshit Anand <10houndoom@gmail.com>
2026-01-22 21:43:35 +0100

    Add Sales class to handle sales details

---
+++ src/main/java/com/prg2025ta/project/examinationpgr2025ta/SalesClass.java
@@ -0,0 +1,21 @@
+package com.prg2025ta.project.examinationpgr2025ta;
+import java.util.Arrays;
+import java.util.List;
+
+public class Sales {
+
+    static String customerName;
+    static String paymentMethod;
+    static List<String> productsBought;
+    static double total;
+
+    public static void main(String[] args) {
+        // No values assigned for now as benjamin asked 
+
+        // Display sale details (will show default null/0 for now)
+        System.out.println("Customer Name: " + customerName);
+        System.out.println("Payment Method: " + paymentMethod);
+        System.out.println("Products Bought: " + productsBought);
+        System.out.println("Total Amount: $" + total);
+    }
+}
