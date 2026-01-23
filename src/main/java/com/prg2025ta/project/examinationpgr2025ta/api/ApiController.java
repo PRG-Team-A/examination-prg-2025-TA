40d71618fca6f778e3b2f25a6eac196e4ede5099
Benjamin Lang <benjamin.lang@srh.university.de>
2026-01-23 11:37:22 +0100

    web server is running successfully

--- src/main/java/com/prg2025ta/project/examinationpgr2025ta/api/ApiController.java
+++ src/main/java/com/prg2025ta/project/examinationpgr2025ta/api/ApiController.java
@@ -1,19 +1,20 @@
 package com.prg2025ta.project.examinationpgr2025ta.api;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
 @RestController
+@SpringBootApplication
 public class ApiController {
 
-    @Value("${server.port}")
-
     public static void main(String[] args) {
-        SpringApplication.run(ProductsApi.class, args);
+        SpringApplication.run(ApiController.class, args);
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "<h1>Index</h1>";
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,9 @@
 module com.prg2025ta.project.examinationpgr2025ta {
-    requires javafx.controls;
-    requires javafx.fxml;
+    requires spring.boot.autoconfigure;
     requires spring.boot;
     requires spring.web;
-    requires spring.boot.autoconfigure;
-    requires spring.beans;
-
-
-    opens com.prg2025ta.project.examinationpgr2025ta to javafx.fxml;
     exports com.prg2025ta.project.examinationpgr2025ta.products;
     exports com.prg2025ta.project.examinationpgr2025ta.exceptions;
     exports com.prg2025ta.project.examinationpgr2025ta;
+    exports com.prg2025ta.project.examinationpgr2025ta.api;
 }
