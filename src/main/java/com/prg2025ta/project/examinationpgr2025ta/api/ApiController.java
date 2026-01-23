eb2b2371effef12906bd4f67869c5bcab8125b28
Benjamin Lang <benjamin.lang@srh.university.de>
2026-01-23 13:21:20 +0100

    web server is running successfully

--- src/main/java/com/prg2025ta/project/examinationpgr2025ta/api/ApiController.java
+++ src/main/java/com/prg2025ta/project/examinationpgr2025ta/api/ApiController.java
@@ -1,13 +1,26 @@
 package com.prg2025ta.project.examinationpgr2025ta.api;
 
+import com.prg2025ta.project.examinationpgr2025ta.Warehouse;
+import com.prg2025ta.project.examinationpgr2025ta.products.Product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import tools.jackson.databind.ObjectMapper;
+
+import java.util.Map;
 
 @RestController
 @SpringBootApplication
 public class ApiController {
+
+    private static Warehouse warehouse = new Warehouse();
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static void setWarehouse(Warehouse warehouse) {
+        ApiController.warehouse = warehouse;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ApiController.class, args);
@@ -16,5 +29,20 @@ public class ApiController {
     @GetMapping("/")
     public String index() {
         return "<h1>Index</h1>";
+    }
+
+    @GetMapping("/list/products")
+    public String listProducts() {
+        Map<Product, Integer> productsInStock = warehouse.getProductsInStock();
+        return objectMapper.writeValueAsString(productsInStock);
+    }
+
+    @PutMapping("/product")
+    public String putProduct(@Validated @RequestBody Product product, Errors errors) {
+        errors.getAllErrors().iterator().forEachRemaining(objectError -> {
+            System.out.println(objectError.toString());
+        });
+        warehouse.acceptDelivery(product);
+        return "{'status': 'success'}";
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.prg2025ta.project.examinationpgr2025ta {
     requires spring.boot.autoconfigure;
     requires spring.boot;
     requires spring.web;
+    requires tools.jackson.databind;
+    requires spring.context;
     exports com.prg2025ta.project.examinationpgr2025ta.products;
     exports com.prg2025ta.project.examinationpgr2025ta.exceptions;
     exports com.prg2025ta.project.examinationpgr2025ta;
