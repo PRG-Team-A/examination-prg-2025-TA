53be935b4487dd249dfbba59e152aaa84d7227ca
Akshit Anand <10houndoom@gmail.com>
2026-01-23 16:09:55 +0100

    Refactor SalesClass to use instance variables
    Removed static fields and added instance variables for customer details. (Supposed to add constructor before tomorrow)

--- src/main/java/com/prg2025ta/project/examinationpgr2025ta/SalesClass.java
+++ src/main/java/com/prg2025ta/project/examinationpgr2025ta/SalesClass.java
@@ -3,17 +3,17 @@ import java.util.Arrays;
 import java.util.List;
 
 public class SalesClass {
-
-    static String customerName;
-    static String paymentMethod;
-    static List<String> productsBought;
-    static double total;
+//Add a constructor
+//Add test products (Optional)
+    private int customerID;
+    private String paymentMethod;
+    private List<String> productsBought;
+    private double total;
 
     public static void main(String[] args) {
-        // No values assigned for now as benjamin asked 
 
         // Display sale details (will show default null/0 for now)
-        System.out.println("Customer Name: " + customerName);
+        System.out.println("Customer Name: " + customerID);
         System.out.println("Payment Method: " + paymentMethod);
         System.out.println("Products Bought: " + productsBought);
         System.out.println("Total Amount: $" + total);
