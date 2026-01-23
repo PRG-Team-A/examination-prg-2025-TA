71ba739e9e9ab6c5342bb4e86e5961b17c12d044
Benjamin Lang <benjamin.lang@stud.srh-university.de>
2026-01-23 18:56:23 +0100

    basics are done

---
+++ src/main/java/com/prg2025ta/project/examinationpgr2025ta/api/PlainController.java
@@ -0,0 +1,13 @@
+package com.prg2025ta.project.examinationpgr2025ta.api;
+
+import com.prg2025ta.project.examinationpgr2025ta.api.models.ProductModel;
+import com.prg2025ta.project.examinationpgr2025ta.products.GroceryProduct;
+import com.prg2025ta.project.examinationpgr2025ta.products.Product;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PlainController {
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -13,6 +13,7 @@ module com.prg2025ta.project.examinationpgr2025ta {
     exports com.prg2025ta.project.examinationpgr2025ta.exceptions;
     exports com.prg2025ta.project.examinationpgr2025ta;
     exports com.prg2025ta.project.examinationpgr2025ta.api;
+    exports com.prg2025ta.project.examinationpgr2025ta.api.models;
 
     opens templates;
 }
