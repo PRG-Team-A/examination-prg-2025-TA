76b0d00de32bbeeeafbbc0e9cb1f4c1badbe4aef
Benjamin Lang <benjamin.lang@stud.srh-university.de>
2026-01-24 13:57:37 +0100

    fix merge conflict

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,13 +9,9 @@ module com.prg2025ta.project.examinationpgr2025ta {
     requires org.apache.tomcat.embed.core;
     requires spring.beans;
     requires spring.webmvc;
-    requires javafx.controls;
-    requires javafx.fxml;
     requires java.sql;
     requires org.jetbrains.annotations;
 
-
-    opens com.prg2025ta.project.examinationpgr2025ta to javafx.fxml;
     exports com.prg2025ta.project.examinationpgr2025ta.products;
     exports com.prg2025ta.project.examinationpgr2025ta.exceptions;
     exports com.prg2025ta.project.examinationpgr2025ta;
