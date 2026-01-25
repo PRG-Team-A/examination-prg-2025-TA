39064643ad9e9a63a9afed592248b21d044c14b8
sowie 43556 <u20221a963@upc.edu.pe>
2026-01-25 19:02:23 +0100

    feat: add Spring Boot main application class (ExaminationPgr2025TaApplication)

---
+++ src/main/java/com/prg2025ta/project/examinationpgr2025ta/ExaminationPgr2025TaApplication.java
@@ -0,0 +1,12 @@
+package com.prg2025ta.project.examinationpgr2025ta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
