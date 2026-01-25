ccd2cee798a7e24b604d230de020698566c0e43c
sowie 43556 <u20221a963@upc.edu.pe>
2026-01-25 17:18:01 +0100

    refactor: remove old JavaFX classes and module-info (replaced by Spring Boot)

--- src/main/java/com/prg2025ta/project/examinationpgr2025ta/MainClass.java
+++
@@ -1,7 +0,0 @@
-package com.prg2025ta.project.examinationpgr2025ta;
-
-public class MainClass {
-    static void main() {
-        throw new UnsupportedOperationException("Main method not yet written");
-    }
-}
