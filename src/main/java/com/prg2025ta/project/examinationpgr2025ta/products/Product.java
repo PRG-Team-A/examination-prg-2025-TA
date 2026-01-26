1d75a7882b53b4318ebe1a8218d5915ba6f3f583
sowie 43556 <u20221a963@upc.edu.pe>
2026-01-26 18:16:24 +0100

    refactor: remove duplicate products package

--- src/main/java/com/prg2025ta/project/examinationpgr2025ta/products/Product.java
+++
@@ -1,9 +0,0 @@
-package com.prg2025ta.project.examinationpgr2025ta.products;
-
-import java.util.UUID;
-
-public interface Product {
-    UUID getUuid();
-    double getPrice();
-    String getDisplayName();
-}
