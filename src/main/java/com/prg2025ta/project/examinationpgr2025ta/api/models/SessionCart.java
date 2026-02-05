5a344df4597173d08d9b290ec77273fdbaf9a46c
Benjamin Lang <benjamin.lang@stud.srh-university.de>
2026-02-05 11:30:15 +0100

    i dont want to type all of that out, it isnt even thaaaat much

--- src/main/java/com/prg2025ta/project/examinationpgr2025ta/api/models/SessionCart.java
+++ src/main/java/com/prg2025ta/project/examinationpgr2025ta/api/models/SessionCart.java
@@ -13,14 +13,17 @@ import java.util.concurrent.atomic.AtomicReference;
 @SessionScope
 public class SessionCart {
     private SalesClass sale;
+    private boolean hasBeenInitialized = false;
 
     public void initialize() {
+        if (hasBeenInitialized) return;
         final int customerId = -5;
         final String paymentMethod = "";
         final List<Product> productsBought = new ArrayList<>();
         final double total = 0.0;
 
         sale = new SalesClass(customerId, paymentMethod, productsBought, total);
+        hasBeenInitialized = true;
     }
 
     public void addProductToCart(Product product) {
@@ -33,5 +36,18 @@ public class SessionCart {
             total.updateAndGet(v -> (double) (v + product.getPrice()));
         });
         return total.get();
+    }
+
+    public void setCustomerId(int customerId) {
+        this.sale.setCustomerID(customerId);
+    }
+    public void setPaymentMethod(String paymentMethod) {
+        this.sale.setPaymentMethod(paymentMethod);
+    }
+    public String getPaymentMethod() {
+        return this.sale.getPaymentMethod();
+    }
+    public List<Product> getProductsBought() {
+        return this.sale.getProductsBought();
     }
 }
