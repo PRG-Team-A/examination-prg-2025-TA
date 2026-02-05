8bbd85c2449c3850ab9c60c687e6a29291bab37f
Benjamin Lang <benjamin.lang@stud.srh-university.de>
2026-02-05 11:15:57 +0100

    made app load from database

--- src/main/java/com/prg2025ta/project/examinationpgr2025ta/api/ApiApplication.java
+++ src/main/java/com/prg2025ta/project/examinationpgr2025ta/api/ApiApplication.java
@@ -5,12 +5,15 @@ import com.prg2025ta.project.examinationpgr2025ta.database.DatabaseOperations;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.SQLException;
+
 @SpringBootApplication
 public class ApiApplication {
     public static Warehouse warehouse;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         warehouse = new Warehouse();
+        warehouse.loadFromDatabase(DatabaseOperations.getInstance());
         SpringApplication.run(ApiApplication.class, args);
     }
 }
