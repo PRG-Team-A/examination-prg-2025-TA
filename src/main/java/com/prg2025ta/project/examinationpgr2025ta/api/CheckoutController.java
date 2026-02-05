3e3590bfb7f6b6c26a34e6b4bbcee3cef6599d4e
Benjamin Lang <benjamin.lang@stud.srh-university.de>
2026-02-05 11:29:44 +0100

    creating a cart for each session

--- src/main/java/com/prg2025ta/project/examinationpgr2025ta/api/CheckoutController.java
+++ src/main/java/com/prg2025ta/project/examinationpgr2025ta/api/CheckoutController.java
@@ -1,7 +1,9 @@
 package com.prg2025ta.project.examinationpgr2025ta.api;
 
 import com.prg2025ta.project.examinationpgr2025ta.api.models.ProductModel;
+import com.prg2025ta.project.examinationpgr2025ta.api.models.SessionCart;
 import com.prg2025ta.project.examinationpgr2025ta.products.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,12 @@ import static com.prg2025ta.project.examinationpgr2025ta.api.ProductController.g
 
 @Controller
 public class CheckoutController {
+    private final SessionCart sessionCart;
+
+    @Autowired
+    public CheckoutController(SessionCart sessionCart) {
+        this.sessionCart = sessionCart;
+    }
 
     @GetMapping("/cart")
     public String cart(Model model) {
@@ -20,6 +28,7 @@ public class CheckoutController {
         Map<Product, Integer> productsInStock = ApiApplication.warehouse.getProductsInStock();
         List<ProductModel> productModelList = getProductsAsModel(productsInStock.keySet().stream().toList());
         model.addAttribute("products", productModelList);
+        model.addAttribute("productsInCart", sessionCart.getProductsBought());
 
         return "cart";
     }
