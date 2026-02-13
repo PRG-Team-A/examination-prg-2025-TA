f23c8263819f73b7b230c72d56bca3ec54610244
Benjamin Lang <benjamin.lang@stud.srh-university.de>
2026-02-13 08:40:13 +0100

    Revert "Merge branch 'main' into 23-integration-of-our-project-with-the-spring-boot-interface"
    This reverts commit 16aa088769115286b9fef644e7250c3b7b6b4001, reversing
    changes made to b7a3d05836667f57cab847ba534e1caa73e18850.

--- src/main/java/com/prg2025ta/project/examinationpgr2025ta/SalesClass.java
+++ src/main/java/com/prg2025ta/project/examinationpgr2025ta/SalesClass.java
@@ -5,7 +5,6 @@ import com.prg2025ta.project.examinationpgr2025ta.products.Product;
 
 public class SalesClass {
 
-    private int saleId;
     private int customerID;
     private String paymentMethod;
     private List<Product> productsBought;
@@ -43,32 +42,11 @@ public class SalesClass {
         this.total = total;
     }
 
-    public int getSaleId() { return saleId; }
-
     // Constructor
     public SalesClass(int customerID, String paymentMethod, List<Product> productsBought, double total) {
-        this(-1, customerID, paymentMethod, productsBought, total);
-    }
-    public SalesClass(int saleId, int customerID, String paymentMethod, List<Product> productsBought, double total) {
         this.customerID = customerID;
         this.paymentMethod = paymentMethod;
         this.productsBought = productsBought;
         this.total = total;
-    }
-
-    @Override
-    public int hashCode() {
-        return Integer.hashCode(saleId);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj.getClass() != this.getClass()) return false;
-
-        final SalesClass other = (SalesClass) obj;
-        if (other.getSaleId() != this.getSaleId()) return false;
-
-        return true;
     }
 }
